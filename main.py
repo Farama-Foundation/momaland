7f0eb2b50016cca474c423ca8ecf5c4b53913b25
Tuireann <mannion.patrick@gmail.com>
2023-06-19 15:05:37 +0100

    Updated MOBPD

--- main.py
+++ main.py
@@ -7,7 +7,19 @@ from envs.beach_domain import beach_domain
 def train():
     """Train a random agent on the beach domain."""
     num_agents = 5
-    mobpd_env = beach_domain.parallel_env(sections=3, capacity=2, num_agents=num_agents, num_types=3)
+    # mobpd_env = beach_domain.parallel_env(sections=3, capacity=2, num_agents=num_agents, type_distribution=[0.5, 0.5])
+    # mobpd_env = beach_domain.parallel_env(sections=3, capacity=2, num_agents=num_agents, type_distribution=[0.333, 0.333, 0.334])
+
+    # mobpd_env = beach_domain.parallel_env(
+    #     sections=3, capacity=2, num_agents=num_agents, type_distribution=[0.333, 0.333, 0.334],
+    #     position_distribution=[0.5, 0.5, 1]
+    # )
+
+    mobpd_env = beach_domain.parallel_env(
+        sections=3, capacity=2, num_agents=num_agents, type_distribution=[0.5, 0.5],
+        position_distribution=[0.5, 0.5, 1], num_timesteps=1, reward_scheme="global"
+    )
+
     done = False
     while not done:
         actions = choices([beach_domain.LEFT, beach_domain.RIGHT, beach_domain.STAY], k=num_agents)
--- momadm_benchmarks/envs/beach_domain/beach_domain.py
+++ momadm_benchmarks/envs/beach_domain/beach_domain.py
@@ -62,13 +62,13 @@ class parallel_env(ParallelEnv):
 
     def __init__(
         self,
+        num_timesteps=10,
+        num_agents=100,
+        reward_scheme="local",
         sections=6,
         capacity=10,
-        num_agents=100,
-        init_position="random",
-        init_type="random",
-        num_types=2,
-        num_timesteps=10,
+        type_distribution=(0.5, 0.5),
+        position_distribution=None,
         render_mode=None,
     ):
         """Initializes the beach domain.
@@ -77,20 +77,23 @@ class parallel_env(ParallelEnv):
             sections: TODO
             capacity: TODO
             num_agents: number of agents in the domain
-            init_position: TODO
-            init_type: TODO
-            num_types: TODO
+            type_distribution: TODO # assume that there is an even mixing between two agent types unless specified
+            position_distribution: TODO # assume that agents ae evenly distributed among sections unless specified
             num_timesteps: TODO
             render_mode: render mode
+            reward_scheme: TODO # global or local rewards
         """
+        self.reward_scheme = reward_scheme
         self.sections = sections
         # TODO Extend to distinct capacities per section?
         self.resource_capacities = [capacity for _ in range(sections)]
-        self.num_types = num_types
         self.num_timesteps = num_timesteps
         self.episode_num = 0
-        self.init_position = init_position
-        self.init_type = init_type
+        self.type_distribution = type_distribution
+        assert len(position_distribution) == self.sections, \
+            "number of sections should be equal to the length of the provided position_distribution:"
+        self.position_distribution = position_distribution
+
         self.render_mode = render_mode
         self.possible_agents = ["agent_" + str(r) for r in range(num_agents)]
         self.agents = self.possible_agents[:]
@@ -159,11 +162,18 @@ class parallel_env(ParallelEnv):
 
     def init_state(self):
         """Initializes the state of the environment. This is called by reset()."""
-        if self.init_type == "random":
-            types = [random.randint(0, self.num_types - 1) for _ in self.agents]
-        if self.init_position == "random":
-            state = [random.randint(0, self.sections - 1) for _ in self.agents]
-        return types, state
+        types = random.choices(
+            [i for i in range(len(self.type_distribution))], weights=self.type_distribution, k=self.num_agents
+        )
+
+        if self.position_distribution is None:
+            positions = [random.randint(0, self.sections - 1) for _ in self.agents]
+        else:
+            positions = random.choices(
+                [i for i in range(self.sections)], weights=self.position_distribution, k=self.num_agents
+            )
+        print("types, positions", types, positions)
+        return types, positions
 
     def step(self, actions):
         """Steps in the environment.
@@ -190,27 +200,31 @@ class parallel_env(ParallelEnv):
             self.state[i] = min(self.sections - 1, max(self.state[i] + act, 0))
 
         section_consumptions = np.zeros(self.sections)
-        section_agent_types = np.zeros((self.num_types, self.sections))
+        section_agent_types = np.zeros((self.sections, len(self.type_distribution)))
 
         for i in range(len(self.agents)):
             section_consumptions[self.state[i]] += 1
+            print(i, self.state[i], self.types[i])
             section_agent_types[self.state[i]][self.types[i]] += 1
 
+        # print(section_agent_types)
         self.episode_num += 1
 
         env_termination = self.episode_num >= self.num_timesteps
         reward_per_section = np.zeros((self.sections, NUM_OBJECTIVES))
 
-        # TODO split in separate functions
         if env_termination:
-            for i in range(self.sections):
-                lr_capacity = section_consumptions[i] * np.exp(-section_consumptions[i] / self.resource_capacities[i])
-                t = section_agent_types[i]
-                if sum(t) > 0:
-                    lr_mixture = min(t) / sum(t)
-                else:
-                    lr_mixture = 0
-                reward_per_section[i] = np.array([lr_capacity, lr_mixture])
+            if self.reward_scheme == "local":
+                for i in range(self.sections):
+                    lr_capacity = local_capacity_reward(self.resource_capacities[i], section_consumptions[i])
+                    lr_mixture = local_mixture_reward(section_agent_types[i])
+                    reward_per_section[i] = np.array([lr_capacity, lr_mixture])
+
+            elif self.reward_scheme == "global":
+                g_capacity = global_capacity_reward(self.resource_capacities, section_consumptions)
+                g_mixture = global_mixture_reward(section_agent_types)
+                reward_per_section = np.array([[g_capacity, g_mixture]] * self.sections)
+                print("reward_per_section", reward_per_section)
 
         # Obs: agent type, section id, section capacity, section consumption, % of agents of current type
         observations = {agent: None for agent in self.agents}
@@ -241,3 +255,29 @@ class parallel_env(ParallelEnv):
             self.render()
 
         return observations, rewards, env_termination, infos
+
+
+def global_capacity_reward(capacities, consumptions):
+    global_capacity_r = 0
+    for i in range(len(capacities)):
+        global_capacity_r += local_capacity_reward(capacities[i], consumptions[i])
+    return global_capacity_r
+
+
+def local_capacity_reward(capacity, consumption):
+    # TODO make capacity lookup table to save CPU!
+    return consumption * np.exp(-consumption / capacity)
+
+
+def global_mixture_reward(section_agent_types):
+    sum_local_mix = 0
+    for i in range(len(section_agent_types)):
+        sum_local_mix += local_mixture_reward(section_agent_types[i])
+    return sum_local_mix / len(section_agent_types)
+
+
+def local_mixture_reward(types):
+    lr_mixture = 0
+    if sum(types) > 0:
+        lr_mixture = min(types) / sum(types)
+    return lr_mixture
