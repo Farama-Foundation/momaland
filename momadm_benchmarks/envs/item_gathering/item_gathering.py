015849bab6509e1182ae794d3998fca51467f5db
Tuireann <mannion.patrick@gmail.com>
2023-09-05 11:42:01 +0100

    Fixed comment at start of item_gathering.py

--- momadm_benchmarks/envs/item_gathering/item_gathering.py
+++ momadm_benchmarks/envs/item_gathering/item_gathering.py
@@ -1,7 +1,5 @@
 """Item Gathering environment.
 
-From J[16:32] Mannion, Patrick
-
 Johan Källström and Fredrik Heintz. 2019. Tunable Dynamics in Agent-Based Simulation using Multi-Objective
 Reinforcement Learning. Presented at the Adaptive and Learning Agents Workshop at AAMAS 2019.
 https://liu.diva-portal.org/smash/record.jsf?pid=diva2%3A1362933&dswid=9018
