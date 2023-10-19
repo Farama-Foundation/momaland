170e17a0559e18d6c1d8fbffa001a0d414dfe7f2
Roxana Radulescu <8026679+rradules@users.noreply.github.com>
2023-10-19 15:42:43 +0200

    added item gathering to the tests

--- tests/all_modules.py
+++ tests/all_modules.py
@@ -1,6 +1,5 @@
 from momadm_benchmarks.envs.beach_domain import mobeach_v0
+from momadm_benchmarks.envs.item_gathering import moitemgather_v0
 
 
-all_environments = {
-    "mobeach_v0": mobeach_v0,
-}
+all_environments = {"mobeach_v0": mobeach_v0, "moitemgather_v0": moitemgather_v0}
