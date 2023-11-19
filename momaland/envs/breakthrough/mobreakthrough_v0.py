39ff0cf3ea3205e2283b9ba017d5c06644792a75
threepwoody <hendrik.baier@gmail.com>
2023-11-19 15:21:03 +0100

    Fix imports

--- momaland/envs/breakthrough/mobreakthrough_v0.py
+++ momaland/envs/breakthrough/mobreakthrough_v0.py
@@ -1,5 +1,5 @@
 """Breakthrough environment with multiple objectives."""
-from momadm_benchmarks.envs.breakthrough.breakthrough import env, parallel_env, raw_env
+from momaland.envs.breakthrough.breakthrough import env, parallel_env, raw_env
 
 
 __all__ = ["env", "raw_env", "parallel_env"]
