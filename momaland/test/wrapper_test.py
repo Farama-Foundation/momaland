aa61182a8219ba076242fcec7085e313d43e7177
umutucak <umutucak@proton.me>
2023-11-23 16:33:14 +0100

    aec & prll linearization tests

--- momaland/test/wrapper_test.py
+++ momaland/test/wrapper_test.py
@@ -1,80 +1,74 @@
 """Testing file for the wrappers."""
+import numpy as np
+
 import momaland.utils.aec_wrappers as AECWrappers
 import momaland.utils.parallel_wrappers as ParallelWrappers
-from momaland.envs.multiwalker import momultiwalker_v0 as _env
 
 
-# def parallel_test():
-#     """Full ParallelEnv lifecycle for testing wrappers."""
-#     env = _env.parallel_env(shared_reward=False)
-#     weights = {env.possible_agents[0]: [0.2, 0.6, 0.2], env.possible_agents[2]: [0.1, 0.3, 0.6]}
-#     indices = {env.possible_agents[0]: [0], env.possible_agents[1]: [0, 1], env.possible_agents[2]: [0]}
-#     env = ParallelWrappers.LinearizeReward(env, weights)
-#     env = ParallelWrappers.NormalizeReward(env, indices)
+def aec_linearization_test(env_module):
+    env = env_module.env()
+    weights = {
+        env.possible_agents[0]: np.random.dirichlet(np.ones(env.reward_space(env.possible_agents[0]).shape[0]), size=1)[0],
+        env.possible_agents[1]: np.random.dirichlet(np.ones(env.reward_space(env.possible_agents[1]).shape[0]), size=1)[0],
+    }
+    env = AECWrappers.LinearizeReward(env, weights)
+    env.reset(seed=42)
+    for agent in env.agent_iter():
+        _, reward, termination, truncation, _ = env.last()
+        if agent is env.possible_agents[0] or agent is env.possible_agents[1]:
+            assert len(reward) == 1, "Returned reward should be a scalar value."
+        if termination or truncation:
+            action = None
+        else:
+            action = env.action_space(agent).sample()  # this is where you would insert your policy
+        env.step(action)
+    env.close()
 
-#     _, _ = env.reset(seed=42)
-#     while env.agents:
-#         # print(env.agents)
-#         # this is where you would insert your policy
-#         actions = {agent: env.action_space(agent).sample() for agent in env.agents}
 
-#         _, rewards, _, _, _ = env.step(actions)
-#         for key, value in rewards.items():
-#             print(key, value)
-#         print("===")
-#     env.close()
-
-
-# def aec_test():
-#     """Full AECEnv lifecycle for testing wrappers."""
-#     env = _env.env(shared_reward=False)
-#     weights = {env.possible_agents[0]: [0.2, 0.6, 0.2], env.possible_agents[2]: [0.1, 0.3, 0.6]}
-#     indices = {env.possible_agents[0]: [0], env.possible_agents[1]: [0, 1], env.possible_agents[2]: [0]}
-#     env = AECWrappers.LinearizeReward(env, weights)
-#     env = AECWrappers.NormalizeReward(env, indices)
-
-#     env.reset(seed=42)
-
-#     for agent in env.agent_iter():
-#         _, reward, termination, truncation, _ = env.last()
-#         print(agent, reward)
-#         if termination or truncation:
-#             action = None
-#         else:
-#             action = env.action_space(agent).sample()  # this is where you would insert your policy
-
-#         env.step(action)
-#     env.close()
-
-def aec_normalization_test(env):
+def aec_normalization_test(env_module):
     pass
 
-def aec_linearization_test(env):
+
+def parallel_linearization_test(env_module):
+    env = env_module.parallel_env()
+    weights = {
+        env.possible_agents[0]: np.random.dirichlet(np.ones(env.reward_space(env.possible_agents[0]).shape[0]), size=1)[0],
+        env.possible_agents[1]: np.random.dirichlet(np.ones(env.reward_space(env.possible_agents[1]).shape[0]), size=1)[0],
+    }
+    env = ParallelWrappers.LinearizeReward(env, weights)
+    _, _ = env.reset(seed=42)
+    while env.agents:
+        actions = {agent: env.action_space(agent).sample() for agent in env.agents}
+        _, reward, _, _, _ = env.step(actions)
+        assert (
+            len(reward[env.possible_agents[0]]) == 1 and len(reward[env.possible_agents[1]]) == 1
+        ), "Returned reward should be a scalar value."
+    env.close()
+
+
+def parallel_normalization_test(env_module):
     pass
 
-def parallel_normalization_test(env):
-    pass
 
-def parallel_linearization_test(env):
-    pass
-
-def aec_test(env):
+def aec_test(env_module):
     """Testing for the following wrappers for AEC:
     - Normalization
     - Linear Scalarization
     """
-    aec_normalization_test(env)
-    aec_linearization_test(env)
+    aec_normalization_test(env_module)
+    aec_linearization_test(env_module)
 
-def parallel_test(env):
+
+def parallel_test(env_module):
     """Testing for the following wrappers for Parallel:
     - Normalization
     - Linear Scalarization
     """
-    parallel_normalization_test(env)
-    parallel_linearization_test(env)
+    parallel_normalization_test(env_module)
+    parallel_linearization_test(env_module)
 
-def wrapper_test(env):
+
+def wrapper_test(env_module):
     """Wrapper testing for AEC and Parallel environments."""
-    aec_test(env)
-    parallel_test(env)
+    aec_test(env_module)
+    parallel_test(env_module)
