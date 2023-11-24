7614bf2f9729ca65c2d7278d5eaa4fbc9222b931
umutucak <umutucak@proton.me>
2023-11-24 14:05:20 +0100

    remove legacy base wrapper

--- momaland/utils/parallel_wrappers.py
+++ momaland/utils/parallel_wrappers.py
@@ -5,18 +5,6 @@ from gymnasium.wrappers.normalize import RunningMeanStd
 from pettingzoo.utils.wrappers.base_parallel import BaseParallelWrapper
 
 
-class Wrapper:
-    """Base class for wrappers."""
-
-    def __init__(self, env):
-        """Base wrapper initialization to save the base env."""
-        self._env = env
-
-    def __getattr__(self, name):
-        """Provide proxy access to regular attributes of wrapped objects."""
-        return getattr(self._env, name)
-
-
 class LinearizeReward(BaseParallelWrapper):
     """Convert MO reward vector into scalar SO reward value.
 
