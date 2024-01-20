7cfb04bacbe9e023f935d27b818101426e4359c9
umutucak <umutucak@proton.me>
2024-01-20 14:52:20 +0100

    slightly better auto env naming

--- docs/_scripts/gen_env_docs.py
+++ docs/_scripts/gen_env_docs.py
@@ -125,7 +125,7 @@ for i, env_spec in tqdm(enumerate(filtered_envs)):
         snake_env_name = pattern.sub("_", pascal_env_name).lower()
         # remove what is after the last "-" in snake_env_name e.g. "_v0"
         snake_env_name = snake_env_name[: snake_env_name.rfind("_")]
-        title_env_name = snake_env_name.replace("_", " ").title().replace("Mo-", "MO-")
+        title_env_name = snake_env_name.replace("mo", "mo ").title().replace("Mo ", "MO-").replace(" ", "")
         env_type_title = env_type.replace("_", " ").title()
         related_pages_meta = ""
         if i == 0 or not env_type == str(filtered_envs[i - 1].module).split(".")[2]:
