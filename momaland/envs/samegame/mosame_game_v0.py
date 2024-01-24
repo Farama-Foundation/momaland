62f0f2ad6dd44da8a6a0b53d88617ef5b8e05521
umutucak <umutucak@proton.me>
2024-01-24 19:03:40 +0100

    final env rename + all env doc table update

--- momaland/envs/samegame/mosame_game_v0.py
+++ momaland/envs/samegame/mosame_game_v0.py
@@ -1,5 +1,5 @@
 """Connect 4 environment with multiple objectives."""
-from momaland.envs.samegame.samegame import env, raw_env
+from momaland.envs.samegame.same_game import env, raw_env
 
 
 __all__ = ["env", "raw_env"]
--- momaland/utils/all_modules.py
+++ momaland/utils/all_modules.py
@@ -17,7 +17,7 @@ from momaland.envs.ingenious import moingenious_v0
 from momaland.envs.item_gathering import moitem_gathering_v0
 from momaland.envs.multiwalker import momultiwalker_v0
 from momaland.envs.pistonball import mopistonball_v0
-from momaland.envs.samegame import mosamegame_v0
+from momaland.envs.samegame import mosame_game_v0
 
 
 all_environments = {
@@ -32,6 +32,6 @@ all_environments = {
     "mocongestion_v0": mocongestion_v0,
     "moconnect4_v0": moconnect4_v0,
     "mobreakthrough_v0": mobreakthrough_v0,
-    "mosamegame_v0": mosamegame_v0,
+    "mosame_game_v0": mosame_game_v0,
     "mogem_mining_v0": mogem_mining_v0,
 }
