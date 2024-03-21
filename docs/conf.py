647c29cc467dee31eb0076f709f2ae709a4dbc84
Florian Felten <florian.felten@uni.lu>
2024-03-21 17:57:16 +0100

    Add favicon

--- docs/conf.py
+++ docs/conf.py
@@ -71,7 +71,7 @@ html_theme = "furo"
 html_title = "MOMAland Documentation"
 html_baseurl = "https://momaland.farama.org"
 html_copy_source = False
-# html_favicon = "_static/img/favicon.png"
+html_favicon = "_static/img/favicon.png"
 html_theme_options = {
     "light_logo": "img/momaland.svg",
     "dark_logo": "img/momaland-white.svg",
