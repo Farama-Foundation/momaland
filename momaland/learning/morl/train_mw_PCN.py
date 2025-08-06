9a2d4af6331e6b82af05ab9a566778e588dcf57a
Roxana Rădulescu <8026679+rradules@users.noreply.github.com>
2025-08-06 19:28:59 +0200

    testing the cont wrapper, plus backwards compatibility

--- momaland/learning/morl/train_mw_PCN.py
+++ momaland/learning/morl/train_mw_PCN.py
@@ -5,29 +5,22 @@ import argparse
 import numpy as np
 from morl_baselines.multi_policy.pcn.pcn import PCN
 
-from momaland.learning.morl.sa_env_factory import make_single_agent_ig_env
+from momaland.learning.morl.sa_env_factory import make_single_agent_mw_env
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-seed", type=int, default=42, help="Seed for the agent.")
-    parser.add_argument("-objectives", type=int, default=3, help="Number of objectives/item types for the IG problem.")
-    parser.add_argument("-project", type=str, default="PCN-IG", help="Project name.")
+    parser.add_argument("-project", type=str, default="PCN-MW", help="Project name.")
     args = parser.parse_args()
     seed = args.seed
-    obj = args.objectives
 
-    env = make_single_agent_ig_env(objectives=obj)
-    eval_env = make_single_agent_ig_env(objectives=obj)
+    env = make_single_agent_mw_env()
+    eval_env = make_single_agent_mw_env()
     project_name = args.project
+    obj = 2  # Multiwalker has 2 objectives: stability and speed
 
     ref_point = np.zeros(obj)
-    if obj == 2:
-        max_return = np.array([4.0, 6.0])
-    elif obj == 4:
-        max_return = np.array([3.0, 4.0, 2.0, 1.0])
-    else:
-        max_return = np.array([3.0, 3.0, 2.0])
     print("Reference point: ", ref_point)
 
     agent = PCN(
@@ -39,7 +32,7 @@ if __name__ == "__main__":
         hidden_dim=256,
         batch_size=256,
         project_name=project_name,
-        experiment_name="PCN",
+        experiment_name="MOMAland-MW-Centralised",
         log=False,
     )
     timesteps_per_iter = 10000
@@ -49,5 +42,4 @@ if __name__ == "__main__":
         ref_point=ref_point,
         num_er_episodes=20,
         num_model_updates=50,
-        max_return=max_return,
     )
