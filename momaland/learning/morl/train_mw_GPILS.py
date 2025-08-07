1ddaa8f7eccacc73836e922f9684613b4b354a9d
Roxana Rădulescu <8026679+rradules@users.noreply.github.com>
2025-08-07 17:07:37 +0200

    training scripts

--- momaland/learning/morl/train_mw_GPILS.py
+++ momaland/learning/morl/train_mw_GPILS.py
@@ -3,7 +3,9 @@
 import argparse
 
 import numpy as np
-from morl_baselines.multi_policy.pcn.pcn import PCN
+from morl_baselines.multi_policy.gpi_pd.gpi_pd_continuous_action import (
+    GPILSContinuousAction,
+)
 
 from momaland.learning.morl.sa_env_factory import make_single_agent_mw_env
 
@@ -11,7 +13,7 @@ from momaland.learning.morl.sa_env_factory import make_single_agent_mw_env
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-seed", type=int, default=42, help="Seed for the agent.")
-    parser.add_argument("-project", type=str, default="PCN-MW", help="Project name.")
+    parser.add_argument("-project", type=str, default="GPI-MW", help="Project name.")
     args = parser.parse_args()
     seed = args.seed
 
@@ -20,26 +22,35 @@ if __name__ == "__main__":
     project_name = args.project
     obj = 2  # Multiwalker has 2 objectives: stability and speed
 
-    ref_point = -50 * np.ones(obj)  # Reference point for Multiwalker
+    ref_point = -110 * np.ones(obj)  # Reference point for Multiwalker
     print("Reference point: ", ref_point)
 
-    agent = PCN(
+    agent = GPILSContinuousAction(
         env,
-        seed=seed,
+        learning_rate=3e-4,
         gamma=0.99,
-        scaling_factor=np.ones(obj + 1),
-        learning_rate=1e-3,
-        hidden_dim=512,
         batch_size=256,
-        project_name=project_name,
-        experiment_name="PCN",
+        net_arch=[256, 256],
+        buffer_size=int(2e5),
+        learning_starts=100,
+        min_priority=0.01,
+        per=False,
+        use_gpi=True,
+        gradient_updates=10,
+        tau=1,
         log=True,
+        project_name=project_name,
+        experiment_name="GPI",
+        seed=seed,
     )
+
     timesteps_per_iter = 1000000
+    algo = "gpi-ls"
+
     agent.train(
-        eval_env=eval_env,
         total_timesteps=10 * timesteps_per_iter,
+        eval_env=eval_env,
         ref_point=ref_point,
-        num_er_episodes=50,
-        num_model_updates=100,
+        weight_selection_algo=algo,
+        timesteps_per_iter=timesteps_per_iter,
     )
